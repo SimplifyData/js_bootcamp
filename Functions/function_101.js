323b114620bbdd835ae35fe9ec4cdbd9ac2c9ea6
az <atif.zafar@sds.technology>
2019-10-31 02:51:04 -0400

    Temperature conversion function

--- Functions/function_101.js
+++ Functions/function_101.js
@@ -21,3 +21,17 @@ let val_2 = square(10);
 
 console.log(val_1);
 console.log(val_2);
+
+
+// temp conversion function
+// F to C
+// 32 F = 0 C , 68 F = 20 C
+
+ let convert_f_to_c = function (temperature) {
+     return (temperature - 32) * (5/9)
+ };
+
+ freezing_temp = convert_f_to_c(32);
+sixty_eight_temp =convert_f_to_c(68);
+ console.log(freezing_temp);
+ console.log(sixty_eight_temp);
