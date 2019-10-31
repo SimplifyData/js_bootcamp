d2de5cbae7c701c58bfb0188ceb18fdbee8819e3
az <atif.zafar@sds.technology>
2019-10-31 00:42:02 -0400

    creating boolean variables

---
+++ Basic/boolean.js
@@ -0,0 +1,40 @@
+// === equality operator
+// !== not equal operator
+// < less than operator
+// > greater than operator
+// <= less than equal to operator
+// >= greater than equal to operator
+
+let temperature = 31;
+
+let is_freezing = temperature === 31;
+
+let not_freezing = temperature !== 31;
+
+console.log(is_freezing);
+
+console.log(not_freezing);
+
+let temp_less_than = temperature < 32;
+
+let temp_greater_than = temperature > 32;
+
+console.log(temp_less_than);
+
+console.log(temp_greater_than);
+
+// create age to your age
+// calculate is child - if age 7 or under
+// sernior if age 65 or older
+// print child value
+// print senior value
+
+let age = 27;
+
+let is_child = age <= 7;
+
+let is_senior = age >= 65;
+
+console.log(is_child);
+
+console.log(is_senior);
