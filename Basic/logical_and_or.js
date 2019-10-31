c31c0d567aad935e724a8bb6ecae8a0b329d59db
az <atif.zafar@sds.technology>
2019-10-31 01:51:14 -0400

    creating if else statements with AND and OR Logical Operators

---
+++ Basic/logical_and_or.js
@@ -0,0 +1,13 @@
+
+let temperature = 65;
+let temp_2 = 110;
+// Logical And Operator : &&
+// Logical OR Operator : ||
+
+if (temperature >= 60 && temperature <= 90 ){
+    console.log('It is really nice outside')
+}
+
+if (temp_2 <= 32 || temp_2 >= 100) {
+    console.log('Do not go outside')
+}
