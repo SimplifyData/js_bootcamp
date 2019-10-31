cbfafee1d8326cae4197872f255710afa0e812d8
az <atif.zafar@sds.technology>
2019-10-31 01:59:30 -0400

    creating if else statements with AND and OR Logical Operators

--- Basic/logical_and_or.js
+++ Basic/logical_and_or.js
@@ -11,3 +11,23 @@ if (temperature >= 60 && temperature <= 90 ){
 if (temp_2 <= 32 || temp_2 >= 100) {
     console.log('Do not go outside')
 }
+
+// Challenge Question
+
+//
+
+let is_guest_one_vegan = true;
+let is_guest_two_vegan = false;
+
+// Are Both Vegan = Only offer vegan dishes
+// Are least one vegan? = offer some vegan
+// Else : offer anything on the menu.
+
+if (is_guest_one_vegan === true && is_guest_two_vegan === true) {
+    console.log("Here is the vegan menu.")
+} else if ((is_guest_one_vegan === true && is_guest_two_vegan === false  ) ||
+    (is_guest_one_vegan === false && is_guest_two_vegan === true)) {
+    console.log(' Here is a menu with vegan, and everything else.')
+} else {
+    console.log(" Here is the full menu.")
+}
