0530fa926b165f01aa7ba662cd6bca87223c7f03
az <atif.zafar@sds.technology>
2019-10-31 02:44:24 -0400

    Simple functions

--- Functions/function_101.js
+++ Functions/function_101.js
@@ -1,7 +1,23 @@
+// Function - input (argument), code(), output (return value)
 // a simple function
+
 
 let greet_user = function () {
     console.log('Welcome User!')
-}
+};
 
-greet_user()
+greet_user();
+
+// create a square function
+
+let square = function (num) {
+    //console.log(num * num )
+    return num * num
+
+};
+
+let val_1 = square(3);
+let val_2 = square(10);
+
+console.log(val_1);
+console.log(val_2);
