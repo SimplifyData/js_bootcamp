f2962644222870c8288dd32de8a9256e6cb00a9f
az <atif.zafar@sds.technology>
2019-10-30 23:26:31 -0400

    creating number variables

--- Basic/number.js
+++ Basic/number.js
@@ -1,3 +1,21 @@
-let num = 5.5
+let num = 5.5;
 
-console.log(num)
+console.log(num);
+
+let num_1 = 5.5 -1;
+
+console.log(num_1);
+
+let num_3 = 5.5 * 2;
+
+console.log(num_3);
+
+let num_4 = 5.5 / 2;
+
+console.log(num_4);
+
+let x = 11;
+
+let num_5 = x + 2 * num_3;
+
+console.log(num_5);
