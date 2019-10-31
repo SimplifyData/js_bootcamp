427f556080be012283afaacf0c69638a03abde6d
az <atif.zafar@sds.technology>
2019-10-30 23:14:06 -0400

    created a variable

--- Basic/strings.js
+++ Basic/strings.js
@@ -1,10 +1,16 @@
-let name = 'atif'
+let name = 'atif';
 
-console.log(name)
+console.log(name);
 
 let first_name = 'Atif';
 let last_name = "Z";
 
 let full_name = first_name +' '+ last_name;
 
-console.log(full_name)
+console.log(full_name);
+
+let city = 'Seattle';
+let country = 'USA';
+let location = city+' '+country;
+
+console.log(location);
