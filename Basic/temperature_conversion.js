72a04c5116774dfe6a2dceba9c6559d5236d432b
az <atif.zafar@sds.technology>
2019-10-31 00:24:37 -0400

    creating temperature conversions

---
+++ Basic/temperature_conversion.js
@@ -0,0 +1,9 @@
+let fahrenheit = 32; // 0 in Celsius & 273.15 Kelvin
+
+let celsius = (fahrenheit - 32) * (5/9);
+
+let kelvin = (fahrenheit + 459.67) * (5/9);
+
+console.log(celsius);
+
+console.log(kelvin);
---
+++ Basic/variables.js
@@ -0,0 +1,11 @@
+/* let pet_name = 'Hal';
+let pet_name = 'Spot';
+
+console.log(pet_name); it will throw an error */
+
+// only use let once to define pet name twice
+
+let pet_name = 'Spot';
+pet_name = 'Tom';
+
+console.log(pet_name);
