a0a53e08ca7d24a7d364b37d9c7e15bad7fbd9f1
az <atif.zafar@sds.technology>
2019-10-31 03:26:37 -0400

    defining multiple arguments in functions

--- Functions/arguments.js
+++ Functions/arguments.js
@@ -4,3 +4,22 @@ let add = function (a,b) {
     return a+b
 };
 console.log(add(3,2));
+
+// providing default values for the arguments
+
+let get_score_text = function (name = 'Player',score = 0) {
+    return ('Name: '+ name + ', Score: ' + score)
+};
+
+console.log(get_score_text());
+console.log(get_score_text('Ax', 77));
+
+// challenge problem
+// total, tip_percentage
+
+let total_payment_for_food = function (total, tip_percent) {
+    let tip = total * tip_percent
+    return ('Total: ' + total + ', Tip: ' + tip)
+};
+
+console.log(total_payment_for_food(342,.15));
