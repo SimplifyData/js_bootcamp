314c69de089e789cfeb756cd8abb0cc1ba77aa4b
az <atif.zafar@sds.technology>
2019-10-31 01:14:08 -0400

    creating if else statements with boolean variables

--- Basic/boolean_advanced.js
+++ Basic/boolean_advanced.js
@@ -1,7 +1,38 @@
-let is_account_locked = true;
+let is_account_locked = false;
+
+let user_role = 'admin';
+
 if (is_account_locked) {
     console.log(' Is account locked?')
 }
 else {
     console.log('Welcome!')
+}
+
+// new if else with admin
+
+if (is_account_locked) {
+    console.log(' Is account locked?')
+}
+else if (user_role === 'admin') {
+        console.log('Welcome Admin!')
+    }
+else {
+    console.log('Welcome!')
+}
+
+// It is freezing outside
+// It is hot outside!
+// Go for it. It is pretty nice.
+
+let temperature = 72;
+
+if (temperature <= 32) {
+    console.log(' It is freezing outside!')
+}
+else if (temperature >= 100) {
+    console.log('It is hot outside!')
+}
+else {
+    console.log('It is nice outside!')
 }
