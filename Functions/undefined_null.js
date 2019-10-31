8fca69584d0a01a53c00822c9e585d5a393f3ce4
az <atif.zafar@sds.technology>
2019-10-31 03:06:56 -0400

    undefined cases for values

---
+++ Functions/undefined_null.js
@@ -0,0 +1,26 @@
+// unassigned variables
+let name = 'Jen';
+
+let name_1; // undefined
+
+console.log(name);
+
+console.log(name_1) // name_1 is undefined
+
+// lets check if name is undefined
+
+if (name_1 === undefined){
+    console.log('Please Provide a Name.')
+} else {
+    console.log(name)
+}
+
+// undefined for function arguments
+
+let square = function (num) {
+    console.log(num)
+}
+
+let result = square(); // undefined
+
+console.log(result); // undefined function return value
