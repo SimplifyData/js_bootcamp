e9685d7fde1eb9e5a8d9e90522a1c1c3f225524b
az <atif.zafar@sds.technology>
2019-11-20 17:00:24 -0500

    JS constant variables with examples

---
+++ Objects/constants.js
@@ -0,0 +1,16 @@
+const is_raining = true;
+
+// used where we need a constant number used repeatedly
+
+
+const person = {
+    age: 27
+}
+
+person.age = 28
+
+// person = {} is not possible since person is a constant now
+
+console.log(person);
+
+// this makes code more logical and readable
