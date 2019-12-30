7c1ff7b9212805d1fd52dcc76149bca3c9124f59
az <atif.zafar@sds.technology>
2019-12-30 00:00:45 -0500

    JS Array with examples

---
+++ Objects/var.js
@@ -0,0 +1,14 @@
+var first_name = 'Adam'
+
+first_name= 'Atif'
+
+var first_name = 'Ali'
+// with var js accepts recreating a variable
+// var is function scoped
+console.log(first_name)
+
+const set_name = function () {
+    var first_name = 'Jen'
+}
+
+set_name()
