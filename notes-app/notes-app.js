796ef65f6bcb30804b9aebafa37b66bf07f651e5
az <atif.zafar@sds.technology>
2020-01-01 05:27:22 -0500

    JS HTML & DOM Web Server examples

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,7 +1,16 @@
-console.log("This is from a different file")
+console.log("This is from a different file");
 
 //DOM - document object model
-
-const p = document.querySelector('p')
+// Query and remove
+const p = document.querySelector('p');
 p.remove()
+
+// Query all and remove
+const ps = document.querySelectorAll('p');
+
+ps.forEach(function (p){
+    // p.remove()
+    console.log(p.textContent)
+})
+
 console.log(p)
